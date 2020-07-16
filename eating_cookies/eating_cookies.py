eeb0186b76fdec43e980fffc8d1abd4864d83f0b
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-07-16 18:58:54 -0500

    ...

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,6 @@ Returns: an integer
 import functools
 
 
-@functools.lru_cache()
 def eating_cookies(n):
     if n == 0:
         return 1
@@ -16,6 +15,27 @@ def eating_cookies(n):
     if n == 3:
         return 4
     return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+
+cache = {}
+
+
+# def eating_cookies(n, cache):
+#     if n == 0:
+#         cache[0] = 1
+#         return 1
+#     if n == 1:
+#         cache[1] = 1
+#         return 1
+#     if n == 2:
+#         cache[2] = 2
+#         return 2
+#     if n == 3:
+#         cache[3] = 4
+#         return 4
+#     if n in cache:
+#         return cache[n]
+#     return eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
 
 
 if __name__ == "__main__":
